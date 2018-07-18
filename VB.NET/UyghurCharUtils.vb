994c46204914921b16c375189d2b2f230cb85d72
kerindax <visualuyghur@126.com>
2018-07-18 18:56:11 +0800

    优化

--- VB.NET/UyghurCharUtils.vb
+++ VB.NET/UyghurCharUtils.vb
@@ -1,9 +1,4 @@
-Imports System.Text
 Imports System.Text.RegularExpressions
-''' <summary>
-''' 
-'''2018.07.17
-''' </summary>
 Public Class UyghurCharUtils
     Private U As Integer(,) = {{&H626, &HFE8B, &HFE8B, &HFE8C, &HFE8C, 1}, {&H627, &HFE8D, &HFE8D, &HFE8E, &HFE8E, 0}, {&H6D5, &HFEE9, &HFEE9, &HFEEA, &HFEEA, 0}, {&H628, &HFE8F, &HFE91, &HFE92, &HFE90, 1}, {&H67E, &HFB56, &HFB58, &HFB59, &HFB57, 1}, {&H62A, &HFE95, &HFE97, &HFE98, &HFE96, 1}, {&H62C, &HFE9D, &HFE9F, &HFEA0, &HFE9E, 1}, {&H686, &HFB7A, &HFB7C, &HFB7D, &HFB7B, 1}, {&H62E, &HFEA5, &HFEA7, &HFEA8, &HFEA6, 1}, {&H62F, &HFEA9, &HFEA9, &HFEAA, &HFEAA, 0}, {&H631, &HFEAD, &HFEAD, &HFEAE, &HFEAE, 0}, {&H632, &HFEAF, &HFEAF, &HFEB0, &HFEB0, 0}, {&H698, &HFB8A, &HFB8A, &HFB8B, &HFB8B, 0}, {&H633, &HFEB1, &HFEB3, &HFEB4, &HFEB2, 1}, {&H634, &HFEB5, &HFEB7, &HFEB8, &HFEB6, 1}, {&H63A, &HFECD, &HFECF, &HFED0, &HFECE, 1}, {&H641, &HFED1, &HFED3, &HFED4, &HFED2, 1}, {&H642, &HFED5, &HFED7, &HFED8, &HFED6, 1}, {&H643, &HFED9, &HFEDB, &HFEDC, &HFEDA, 1}, {&H6AF, &HFB92, &HFB94, &HFB95, &HFB93, 1}, {&H6AD, &HFBD3, &HFBD5, &HFBD6, &HFBD4, 1}, {&H644, &HFEDD, &HFEDF, &HFEE0, &HFEDE, 1}, {&H645, &HFEE1, &HFEE3, &HFEE4, &HFEE2, 1}, {&H646, &HFEE5, &HFEE7, &HFEE8, &HFEE6, 1}, {&H6BE, &HFBAA, &HFBAC, &HFBAD, &HFBAB, 1}, {&H648, &HFEED, &HFEED, &HFEEE, &HFEEE, 0}, {&H6C7, &HFBD7, &HFBD7, &HFBD8, &HFBD8, 0}, {&H6C6, &HFBD9, &HFBD9, &HFBDA, &HFBDA, 0}, {&H6C8, &HFBDB, &HFBDB, &HFBDC, &HFBDC, 0}, {&H6CB, &HFBDE, &HFBDE, &HFBDF, &HFBDF, 0}, {&H6D0, &HFBE4, &HFBE6, &HFBE7, &HFBE5, 1}, {&H649, &HFEEF, &HFBE8, &HFBE9, &HFEF0, 1}, {&H64A, &HFEF1, &HFEF3, &HFEF4, &HFEF2, 1}}
     Private reg_str As String
@@ -21,7 +16,7 @@ Public Class UyghurCharUtils
                 Dim returns As String = ""
                 Dim target As String = ""
                 Dim target2 As String = ""
-                Dim ch As String
+                Dim ch As Integer
                 Dim p As Integer
                 Dim length As Integer = str.Length
                 If length > 1 Then
@@ -103,7 +98,6 @@ Public Class UyghurCharUtils
                            End Function)
     End Function
     Private Function _GetCode(source As String, index As Integer) As Integer
-        Dim target As Integer = 0
         If source.Length = 0 Then Return 0
         If index > 5 Then Return AscW(source)
         For i = 0 To 32
